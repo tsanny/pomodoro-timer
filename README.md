1f48df1fb5b1a0319507a6f4eb09e342a31afb11
Sebastian Rugina <dev@srugina.com>
2021-02-10 20:48:28 +0000

    Add schedule_recv, structopt, figlet
    schedule_recv uses a Binary Heap Priority Queue, which is more accurate
    than std::thread::sleep.
    
    structopt makes it easy to accept cli arguements and have a --help.
    
    figlet to match blog post's formatting.
    
    ding.wav shortened to 0.9s to not miss any dings on 1s intervals.

--- README.md
+++ README.md
@@ -14,11 +14,17 @@ Three explicit threads in total are used:
 
 Additionally, reasonable limits have been set on the interval length and count (u16 and u8 respectively).
 
+To further reduce drift, the `schedule_recv` crate was used. Essentially, instead of using `std::thread::sleep`, we use a Binary Heap Priority Queue to track time, which is more accurate. This crate spawns its own thread, so the `main.rs` file no longer has an explicit spawn for the time thread. Now that there was only one MPSC channel communicating once at the end of each interval, changing from the standard library's MPSC to something like `flume` or `crossbeam-channel` did not have a noticeable impact on performance.
+
+`Figlet-rs` was also added to match the formatting of the original for feature-parity, despite lack of legibility when terminal width is low enough to cause line wrapping. Another addition was `structopt` to allow configuration (now including warm up time) in the calling line e.g. `better-interval-timer 5 2 -w 10`, and an informative `--help`. The impact of reading these arguments is not large enough to affect the drift measurement in the performance section below.
+
+To prevent audio from not triggering on each interval end (due to the interval being shorter than the sound), the ding sound effect was also shortened to 0.9s.
+
 ## Performance
 
-Performance was measured by commenting out the code requesting an interval length and count, modifying the input strings in the code instead.
+Performance was measured by using the following command: `time ./better-interval-timer <interval-length> <interval-count>`
 
-We expect the Ideal Time to be the warmup time (10 seconds) + (interval length * interval count) + sound effect at the end (2.856 seconds).
+We expect the Ideal Time to be the warm up time (default 10 seconds) + (interval length * interval count) + sound effect at the end (2.856 seconds).
 
 | Interval length (Seconds) | Interval count | Time (MM:SS.sss) | Ideal Time (MM:SS.ss) | Drift (MM:SS.sss) |       Error (%)       |
 | :-----------------------: | :------------: | :--------------: | :-------------------: | :---------------: | :-------------------: |
@@ -26,6 +32,13 @@ We expect the Ideal Time to be the warmup time (10 seconds) + (interval length *
 |           900             |       02       |    30:13.086     |       30:12.856       |     00:00.230     | **0.0127% (3 s.f.)**  |
 
 Interestingly, it appears the drift error decreases the longer the program is running.
+
+After switching to `schedule_recv`, performance was measured by using the following command: `time ./better-interval-timer <interval-length> <interval-count>`
+
+| Interval length (Seconds) | Interval count | Time (MM:SS.sss) | Ideal Time (MM:SS.ss) | Drift (MM:SS.sss) |         Error (%)         |
+| :-----------------------: | :------------: | :--------------: | :-------------------: | :---------------: | :-----------------------: |
+|            05             |       02       |    00:22.858     |       00:22.856       |     00:00.002     |   0.00875% (3 s.f.)       |
+|           900             |       02       |    30:12.860     |       30:12.856       |     00:00.004     | **0.00000221% (3 s.f.)**  |
 
 ## Comparison
 
@@ -35,9 +48,12 @@ The blog post states:
 
 Giving the benefit of the doubt, I'll assume the author has forgotten to take into account the 10 second warm-up and 2.856 second sound at the end in the claim of "15 extra seconds", so the drift was 2.144 seconds. Assuming a length of 1800 seconds and a count of 1 for the blog post:
 
-|        Program        |    Time    | Ideal Time  |   Drift   |        Error         |
-| :-------------------: | :--------: | :---------: | :-------: | :------------------: |
-|    interval-timer     | ~30:15.000 |  30:12.856  | 00:02.144 |   0.118% (3 s.f.)    |
-| better-interval-timer |  30:13.086 |  30:12.856  | 00:00.230 | **0.0127% (3 s.f.)** |
+|         Program          |    Time    | Ideal Time  |   Drift   |          Error           |
+| :----------------------: | :--------: | :---------: | :-------: | :----------------------: |
+|      interval-timer      | ~30:15.000 |  30:12.856  | 00:02.144 |   0.118% (3 s.f.)        |
+|  better-interval-timer   |  30:13.086 |  30:12.856  | 00:00.230 |   0.0127% (3 s.f.)       |
+| better, w/ schedule_recv |  30:12.860 |  30:12.856  | 00:00.004 | **0.00000221% (3 s.f.)** |
 
-This repository reduces drift error by 89.3% (3 s.f.).
+This repository used to reduce drift by 89.3% (3 s.f.).
+
+With `schedule_recv`, it now reduces drift by 99.813% (3 s.f.).
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,38 @@
+use figlet_rs::FIGfont;
+use schedule_recv::{oneshot_ms, periodic_ms};
 use std::{
-    error::Error,
     include_bytes,
-    io::{self, Write},
-    process::{Command, Stdio},
+    io::Write,
+    process::{exit, Command, Stdio},
     sync::mpsc::channel,
-    thread::{sleep, spawn},
-    time::Duration,
+    thread::spawn,
 };
+use structopt::StructOpt;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut input_interval_length = String::new();
-    let mut input_interval_count = String::new();
+#[derive(StructOpt)]
+struct Cli {
+    /// The length of each interval in seconds, minimum 1
+    #[structopt(default_value = "60")]
+    interval_length: u16,
+    /// The number of intervals (after warm up), minimum 1
+    #[structopt(default_value = "2")]
+    interval_count: u8,
 
-    // clear terminal and set cursor to top
-    print!("\x1B[2J\x1B[1;1H");
-    print!("Enter the length in seconds of each interval (max 65535): ");
-    // ensure prompt above is printed immediately
-    io::stdout().flush()?;
-    io::stdin().read_line(&mut input_interval_length)?;
+    /// The warmup time in seconds, minimum 1
+    #[structopt(short, long, default_value = "10")]
+    warmup_time: u16,
+}
 
-    print!("Enter the count of intervals (max 255): ");
-    io::stdout().flush()?;
-    io::stdin().read_line(&mut input_interval_count)?;
+fn main() {
+    let args = Cli::from_args();
+    let interval_length: u16 = 1.max(args.interval_length);
+    let interval_count: u8 = 1.max(args.interval_count);
+    let warmup_time: u16 = 1.max(args.warmup_time);
 
-    print!("\x1B[2J\x1B[1;1H");
-    // convert input to numbers
-    let interval_length: u16 = input_interval_length
-        .trim()
-        .parse()
-        .expect("Min, max interval length (seconds) of 1,65535");
-    let interval_count: u8 = input_interval_count
-        .trim()
-        .parse()
-        .expect("Min, max interval count of 1,255");
+    // set up figlet with standard font
+    let standard_font = FIGfont::standand().unwrap();
 
-    // create transmitters and receivers to communicate between threads
-    let (time_tx, time_rx) = channel();
+    // create transmitter and receiver to communicate between threads
     let (audio_tx, audio_rx) = channel();
 
     // spawn audio thread
@@ -55,7 +52,6 @@ fn main() -> Result<(), Box<dyn Error>> {
                     .spawn()
                     .expect("failed to spawn aplay, is it installed?")
                     .stdin
-                    .as_ref()
                     .unwrap()
                     .write_all(test)
                     .unwrap();
@@ -63,48 +59,44 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    spawn(move || {
-        // initial warm-up time
-        let mut time_sec = 10;
-        loop {
-            sleep(Duration::from_secs(1));
-            // send new time early to be displayed
-            time_tx.send(time_sec - 1).unwrap();
-            time_sec -= 1;
-            if time_sec == 0 {
-                // reset timer to interval length again
-                time_sec = interval_length;
+    let mut current_interval = 0;
+    // initial warm-up time
+    let mut time_sec = warmup_time;
+    let tick = periodic_ms(1000);
+    loop {
+        tick.recv().unwrap();
+        time_sec -= 1;
+
+        // get minutes as int, so if under 1 minute shows 0
+        let interval_minutes = time_sec / 60;
+        // clear terminal and set cursor to start of first line
+        print!("\x1B[2J\x1B[1;1H");
+        // format time to have 0 before int when below 2sf
+        let text = format!(
+            "Interval {} of {}: {:02}:{:02}",
+            current_interval,
+            interval_count,
+            interval_minutes,
+            time_sec - (interval_minutes * 60)
+        );
+        // pass formatted time to figlet and print
+        let figure = standard_font.convert(text.as_str());
+        println!("{}", figure.unwrap());
+
+        // countdown for this interval has reached zero, play sound and move on
+        if time_sec == 0 {
+            // reset timer to interval length again
+            time_sec = interval_length;
+
+            audio_tx.send(()).unwrap();
+            current_interval += 1;
+            // when current == total + 1 (warm-up), we are done, so exit
+            if current_interval == interval_count + 1 {
+                println!("Finished!");
+                // wait until sound finishes (0.9 seconds)
+                oneshot_ms(900).recv().unwrap();
+                exit(0);
             }
         }
-    });
-
-    let mut current_interval = 0;
-    loop {
-        let _ = time_rx.recv().map(|reply| {
-            // get minutes as int, so if under 1 minute shows 0
-            let interval_minutes = reply / 60;
-            print!("\x1B[2J\x1B[1;1H");
-            // format time to have 0 before int when below 2sf
-            println!(
-                "Interval {} of {}: {:02}:{:02}",
-                current_interval,
-                interval_count,
-                interval_minutes,
-                reply - (interval_minutes * 60)
-            );
-
-            // countdown for this interval has reached zero, play sound and move on
-            if reply == 0 {
-                audio_tx.send(true).unwrap();
-                current_interval += 1;
-                // when current == total + 1 (warm-up), we are done, so exit
-                if current_interval == interval_count + 1 {
-                    println!("Finished!");
-                    // wait until sound finishes (2.856 seconds)
-                    sleep(Duration::from_millis(2856));
-                    std::process::exit(0);
-                }
-            }
-        });
     }
 }
